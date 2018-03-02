f845bfb979ad273540fa4d8babd9c71772c5bdac
Miguel Moreno Baladron <miguel.moreno.baladron@gmail.com>
2018-03-02 00:21:57 +0000

    Some minor changes added

--- app.rb
+++ app.rb
@@ -4,5 +4,7 @@ require_relative('controllers/transaction_controller.rb')
 require_relative('controllers/tags_controller.rb')
 
 get '/' do
+  @transactions = Transaction.all
+  @transactions_total = Transaction.total
   erb(:index)
 end
